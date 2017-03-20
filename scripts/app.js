5705014b8338fb34edb13f85f9c0ef6408d64db3
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-20 14:01:19 +0530

    files changed

--- scripts/app.js
+++ scripts/app.js
@@ -12,26 +12,26 @@ function searchPages() {
 function getAllPages(pageName) {
     //GET for Page Node Details
     if (FB) {
-        var urlCall = "/search?q=" + pageName + "&type=page&access_token=?fields=category,about,cover";
+        var urlCall = "/search?q=" + pageName + "&type=page&access_token=";
         FB.api(urlCall, function(response) {
           if(response.data){
             console.log(response.data);
-              fetchPageDetails(response.data);
+              // fetchPageDetails(response.data);
               renderPage()
           }
         });
     }
 }
 
-function fetchPageDetails(data){
-  var urlCall = "/search?q=" + pageName + "&type=page&access_token=";
-        FB.api(urlCall, function(response) {
-          if(response.data){
-              fetchPageDetails(response.data);
-              renderPage()
-          }
-        });
-}
+// function fetchPageDetails(data){
+//   var urlCall = "/search?q=" + pageName + "&type=page&access_token=";
+//         FB.api(urlCall, function(response) {
+//           if(response.data){
+//               fetchPageDetails(response.data);
+//               renderPage()
+//           }
+//         });
+// }
 function renderPage(){
     var resultsEle = document.getElementById('results');
     var searchbar = document.getElementById('searchbar');
