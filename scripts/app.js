551a971973bf52ad4c1fe96acee4baab4f6f75e5
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-21 00:59:14 +0530

    files changed

--- scripts/app.js
+++ scripts/app.js
@@ -31,9 +31,15 @@ function fetchPageDetails(data){
     var urlCall = "/"+data[i].id + "?fields=category,cover,about,name";
       FB.api(urlCall, function(response) {
         if(response){
+          var favStatus;
           var tempImg = response.cover ? response.cover.source : "assets/fb-art.png";
           searchResults.push(response);
-          var favStatus = searchResults[i].isFav ? true:false;
+          if(searchResults[i].isFav){
+            favStatus = true;
+          }else{
+            favStatus = false;
+          }
+
           searchResults[i].isFav = favStatus;
           var tempItem = '<li class="result-wrp clearfix"><div class="page-image"><img src="'+tempImg+'"></div><div class="res-right-wrp"><div class="page-name">'+ response.name +'</div><div class="page-cat">'+response.category+'</div><div class="page-desc"><p>'+response.about +'</p></div></div><div class="page-fav" data-fav='+favStatus+'onclick="favoriteItem()">LIKE</div></li>'
           pageListData+=tempItem;
