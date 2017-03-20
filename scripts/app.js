21427d5d9d6f615e500aff6db7f9657c1dcfa6af
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-20 22:50:50 +0530

    files changed

--- scripts/app.js
+++ scripts/app.js
@@ -16,37 +16,53 @@ function getAllPages(pageName) {
         FB.api(urlCall, function(response) {
           if(response.data){
             console.log(response.data);
-              // fetchPageDetails(response.data);
-              renderPage()
+              fetchPageDetails(response.data);
+              renderPage(response.data);
           }
         });
     }
 }
 
-// function fetchPageDetails(data){
-//   var urlCall = "/search?q=" + pageName + "&type=page&access_token=";
-//         FB.api(urlCall, function(response) {
-//           if(response.data){
-//               fetchPageDetails(response.data);
-//               renderPage()
-//           }
-//         });
-// }
-function renderPage(){
+function fetchPageDetails(data){
+  var urlCall = "/search?q=" + pageName + "&type=page&access_token=";
+        FB.api(urlCall, function(response) {
+          if(response.data){
+              fetchPageDetails(response.data);
+              renderPage()
+          }
+        });
+}
+function renderPage(data){
     var resultsEle = document.getElementById('results');
     var searchbar = document.getElementById('searchbar');
     var pos = 0;
     var id = setInterval(frame, 5);
     resultsEle.style.display = 'block';
-    function frame() {
-        if (pos == -400) {
-            clearInterval(id);
-        } else {
-            pos--;
-            resultsEle.style.top = pos + 'px';
-            searchbar.style.top = pos / 3 + 'px';
-        }
+    // function frame() {
+    //     if (pos == -400) {
+    //         clearInterval(id);
+    //     } else {
+    //         pos--;
+    //         resultsEle.style.top = pos + 'px';
+    //         searchbar.style.top = pos / 3 + 'px';
+    //     }
+    // }
+    var listItems =[];
+    // Creating the list
+    for(var i=0;i<data.length;i++){
+      var listItem = '<li class="result-wrp clearfix">
+                        <div class="page-image"><img src="'+ +'"></div>
+                        <div class="res-right-wrp">
+                            <div class="page-name">'+ Coca-Cola +'</div>
+                            <div class="page-cat">'+Category+'</div>
+                            <div class="page-desc">
+                                <p>'+Lorem Ipsum+'</p>
+                            </div>
+                        </div>
+                    </li>'
+      listItems.push(listItem);
     }
+
 }
 
 
