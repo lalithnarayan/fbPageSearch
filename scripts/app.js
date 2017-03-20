b24279bfca1dd7be4110d446c32f63f8651a9e5c
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-20 09:22:18 +0530

    files changed

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,8 @@
-var app = angular.module('myApp', ['iso.directives']);
-app.controller('dashboardCntrl', function($scope,$http) {
- 
+var searchElem = document.getElementById('search');
 
- 
-
-  $scope.init = function(){
-     $http.get("https://api.myjson.com/bins/wbsmn")
-        .then(function(response) {
-            $scope.ajbs = response.data.ajbs;
-        });
+function searchPages() {
+  var searchTerm = searchElem.value;
+  if(searchTerm){
+    searchTerm = searchTerm.replace(" ", "");
   }
-
-  $scope.tileBg = function(ajb){
-    var speedStatus = ajb.last_spd_status[0].spd_status;
-    console.log(ajb.last_spd_status[0].spd_status);
-    if(speedStatus === "OK"){
-        return "active";
-    }else{
-        return "error"   
-    }
-  }
-    // Function called on page intialize
-    $scope.init();
-});
+}
