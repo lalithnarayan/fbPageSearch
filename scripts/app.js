78e88f151377d12eb5440d6ece762299b09e4dce
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-20 11:17:43 +0530

    files changed

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,56 @@
-var searchElem = document.getElementById('search');
+(function() {
+    var searchElem = document.getElementById('search');
+    var accessToken = 1861865227430347;
 
-function searchPages() {
-  var searchTerm = searchElem.value;
-  if(searchTerm){
-    searchTerm = searchTerm.replace(" ", "");
-    fetchPageDetails(searchTerm);
-  }
-}
+    function searchPages() {
+        var searchTerm = searchElem.value;
+        if (searchTerm) {
+            searchTerm = searchTerm.replace(" ", "");
+            fetchPageDetails(searchTerm);
+        }
+    }
 
-function fetchPageDetails(pageName){
-  console.log(pageName)
-}
+    function fetchPageDetails(pageName) {
+        //GET for Page Node Details
+        if (FB) {
+            FB.api(urlCall, function(response) {
+                debugger;
+                console.log(response);
+
+            });
+            FB.api('/me', { fields: 'last_name' }, function(response) {
+                console.log(response);
+            });
+        }
+
+        var resultsEle = document.getElementById('results');
+        var searchbar = document.getElementById('searchbar');
+        var pos = 0;
+        var id = setInterval(frame, 5);
+
+        function frame() {
+            if (pos == -400) {
+                clearInterval(id);
+
+            } else {
+                pos--;
+                resultsEle.style.top = pos + 'px';
+                searchbar.style.top = pos / 3 + 'px';
+            }
+        }
+    }
+
+    logInWithFacebook = function() {
+        FB.login(function(response) {
+            if (response.authResponse) {
+                alert('You are logged in &amp; cookie set!');
+                // Now you can redirect the user or do an AJAX request to
+                // a PHP script that grabs the signed request from the cookie.
+            } else {
+                alert('User cancelled login or did not fully authorize.');
+            }
+        });
+        return false;
+    };
+
+})();
