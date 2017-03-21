e8ab39d6b6b388436a3401f76405cb3d72636cae
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-21 07:58:41 +0530

    files changed

--- scripts/app.js
+++ scripts/app.js
@@ -22,38 +22,42 @@ function getAllPages(pageName) {
 }
 
 function fetchPageDetails(data){
-  var pageListData = "";
-  var resultsContainer = document.getElementById('result-holder');
+  
+  
   for(var i=0;i<data.length;i++){
     var urlCall = "/"+data[i].id + "?fields=category,cover,about,name";
       FB.api(urlCall, function(response) {
         if(response){
-          renderPage(response,i)
+           if(response.isFav){
+              response.favStatus = true;
+
+            }else{
+              response.favStatus = false;
+            }
+          searchResults.push(response);
+          renderPage(response);
         }
     });
   }
   
 }
 
-function renderPage(data,i){
-  debugger;
+function renderPage(data){
+  var pageListData = "";
   var favStatus;
-  var tempImg = response.cover ? response.cover.source : "assets/fb-art.png";
-  searchResults.push(response);
-  if(response.isFav){
-    favStatus = true;
-
-  }else{
-    favStatus = false;
-  }
-  var tempItem = '<li class="result-wrp clearfix"><div class="page-image"><img src="'+tempImg+'"></div><div class="res-right-wrp"><div class="page-name">'+ response.name +'</div><div class="page-cat">'+response.category+'</div><div class="page-desc"><p>'+response.about +'</p></div><div class="page-fav" data-fav="'+favStatus+'"onclick="favoriteItem(e)">LIKE</div></div></li>'
-  pageListData+=tempItem;
-  resultsContainer.innerHTML = pageListData;
-  if(index == (searchResults.length-1)){
-    var resultsEle = document.getElementById('results');
-    resultsEle.style.display = 'block';
-  }
+  var resultsContainer = document.getElementById('result-holder');
+  var tempImg = data.cover ? data.cover.source : "assets/fb-art.png";
   
+ 
+  var tempItem = '<li class="result-wrp clearfix"><div class="page-image"><img src="'+tempImg+'"></div><div class="res-right-wrp"><div class="page-name">'+ data.name +'</div><div class="page-cat">'+data.category+'</div><div class="page-desc"><p>'+data.about +'</p></div><div class="page-fav" data-fav="'+response.favStatus+'"onclick="favoriteItem(e)">LIKE</div></div></li>'
+  pageListData+=tempItem;
+
+
+  
+  resultsContainer.innerHTML = pageListData;
+
+  var resultsEle = document.getElementById('results');
+  resultsEle.style.display = 'block';
 }
 
 function favoriteItem(event){
