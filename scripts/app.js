5373b1dd4b16e3a97411cef438b041c4f8f3b746
Lalith narayan Kashyap <lalith@moonraft.com>
2017-03-21 08:03:34 +0530

    files changed

--- scripts/app.js
+++ scripts/app.js
@@ -50,7 +50,7 @@ function renderPage(data){
   var tempImg = data.cover ? data.cover.source : "assets/fb-art.png";
   
  
-  var tempItem = '<li class="result-wrp clearfix"><div class="page-image"><img src="'+tempImg+'"></div><div class="res-right-wrp"><div class="page-name">'+ data.name +'</div><div class="page-cat">'+data.category+'</div><div class="page-desc"><p>'+data.about +'</p></div><div class="page-fav" data-fav="'+response.favStatus+'"onclick="favoriteItem(e)">LIKE</div></div></li>'
+  var tempItem = '<li class="result-wrp clearfix"><div class="page-image"><img src="'+tempImg+'"></div><div class="res-right-wrp"><div class="page-name">'+ data.name +'</div><div class="page-cat">'+data.category+'</div><div class="page-desc"><p>'+data.about +'</p></div><div class="page-fav" data-fav="'+data.favStatus+'"onclick="favoriteItem(e)">LIKE</div></div></li>'
   pageListData+=tempItem;
 
 
